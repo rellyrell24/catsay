5984996072e81484042ff7cdea5784c25f9c1515
rellyrell24 <jg.techie24@gmail.com>
2022-05-15 16:19:38 +0400

    init commit

---
+++ tests/integration_test.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+#[test]
+fn run_with_defaults() -> Result<(), Box<dyn std::error::Error>> {
+    Command::cargo_bin("catsay")
+        .expect("binary exists")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Meow!"));
+    Ok(())
+}
+
+#[test]
+fn fail_on_non_existing_file() -> Result<(), Box<dyn std::error::Error>> {
+    Command::cargo_bin("catsay")
+        .expect("binary exists")
+        .args(&["-f", "no/such/file.txt"])
+        .assert()
+        .failure();
+    Ok(())
+}
